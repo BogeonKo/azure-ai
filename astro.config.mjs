d22d2d35d44d227ca38a0a434ebf2341689cee0c
BogeonKo <157683162+BogeonKo@users.noreply.github.com>
2025-05-30 14:45:10 +0900

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  site: 'https://bogeonko.github.io/azure-portfolio/',
-  base: '/azure-portfolio/',
-  integrations: [tailwind()],
-});
